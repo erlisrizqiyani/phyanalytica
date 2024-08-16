a553fd76f22298b10735b5f54554f2eeaa5e0f75
Erlis Rizqiyani <101242934+erlisrizqiyani@users.noreply.github.com>
2024-08-16 20:42:09 +0700

    final

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -6,15 +6,17 @@ export default function BlogLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-      <div className="flex w-full h-screen">
+    <section className="container mx-auto max-w-7xl pt-16 px-6 gap-8 flex-grow">
+      <div className="flex w-full ">
       <img
-          className="absolute top-0 left-0 w-full object-cover"
+          className="absolute top-0 left-0 w-full sm:h-80 h-60 object-cover "
           src="/Bg-header.png"
         />
-        
       </div>
+      <div>
       {children}
+
+      </div>
     </section>
   );
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,47 +1,69 @@
-// Add this at the top of your file
-'use client';
+"use client";
 
 import "@/styles/Contact.css";
 import { title } from "@/components/primitives";
-import { FormEvent } from 'react';
+import { FormEvent } from "react";
+import { FaPhone, FaEnvelope } from "react-icons/fa";
+import { MdLocationOn } from "react-icons/md";
+import Footer from "@/components/footer";
 
 export default function ContactUs() {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
 
-    const response = await fetch('/api/send_email', {
-      method: 'POST',
+    const response = await fetch("/api/send_email", {
+      method: "POST",
       body: JSON.stringify({
-        name: formData.get('name'),
-        email: formData.get('email'),
-        phone: formData.get('phone'),
-        message: formData.get('message'),
+        name: formData.get("name"),
+        email: formData.get("email"),
+        phone: formData.get("phone"),
+        message: formData.get("message"),
       }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     if (response.ok) {
-      alert('Email sent successfully!');
+      alert("Email sent successfully!");
     } else {
-      alert('Failed to send email.');
+      alert("Failed to send email.");
     }
   };
 
   return (
-    <div className="contact-container -mt-80">
+    <section>
+      <div className="overlay-text-container">
+        <h1 className="overlay-text">Get In Touch</h1>
+        <h2 className="overlay-subtext -mt-4">Comprehensive Data Consulting Services</h2>
+      </div>
+      <div className="contact-container mx-auto mt-60">
       <div className="company-details">
         <h1 className={title()}>Contact Us</h1>
-        <p>We would like to know more about your request. Let's schedule a free estimation call and discuss the next steps.</p>
-        <p>Fill out the form or email us at <a href="mailto:syayakay@gmail.com">syayakay@gmail.com</a></p>
-        <p>Or call us at <a href="tel:+6285156162840">0851-5616-2840</a></p>
+        <p>
+          We would like to know more about your request. Let's schedule a free
+          estimation call and discuss the next steps.
+        </p>
+        <p>
+          <FaEnvelope className="icon ml" /> Email us at{" "}
+          <a href="mailto:syayakay@gmail.com" className="ml-2">syayakay@gmail.com</a>
+        </p>
+        <p>
+          <FaPhone className="icon" /> Call us at{" "}
+          <a href="tel:+6285156162840" className="ml-2">0851-5616-2840</a>
+        </p>
 
         <div className="company-addresses">
           <h4>Head Office</h4>
-          <p><strong>Germany</strong> 🇩🇪<br />Hattingerstr 273 44795, Bochum NRW Germany</p>
-          <p><strong>Indonesia</strong> 🇮🇩<br />Jalan Kaliurang, KM 5 No.7A, DIY, Indonesia</p>
+          <div className="address-section">
+            <MdLocationOn className="icon" />
+            Hattingerstr 273 44795, Bochum NRW Germany
+          </div>
+          <div className="address-section">
+            <MdLocationOn className="icon" />
+            Jalan Kaliurang, KM 5 No.7A, DIY, Indonesia
+          </div>
         </div>
       </div>
 
@@ -51,14 +73,25 @@ export default function ContactUs() {
           <input type="text" name="name" placeholder="Name" required />
           <input type="email" name="email" placeholder="Email" required />
           <input type="text" name="phone" placeholder="Phone" />
-          <textarea name="message" placeholder="Your Message" required></textarea>
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            required
+          ></textarea>
           {/* <input type="file" name="attachment" /> */}
           <label>
-            <input type="checkbox" name="consent" required /> I consent to processing my personal data according to the Privacy Policy
+            <input type="checkbox" name="consent" required /> I consent to
+            processing my personal data according to the Privacy Policy
           </label>
           <button type="submit">Send Message</button>
         </form>
       </div>
     </div>
+
+    <div className="absolute left-0 w-full mt-32">
+      <Footer/>
+    </div>
+    </section>
+    
   );
 }
