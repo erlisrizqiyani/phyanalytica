4c10eb3f76f662b68e06126db3bc743ee1f4b63a
Erlis Rizqiyani <101242934+erlisrizqiyani@users.noreply.github.com>
2024-08-26 00:13:08 +0700

    update cookie

--- components/cookie.tsx
+++ components/cookie.tsx
@@ -19,9 +19,9 @@ const CookieConsent = () => {
         <h2 className="text-lg font-bold mb-2">We value your Privacy</h2>
         <p>
           This website stores cookies on your computer. These cookies are used to collect information about how you interact with our website and allow us to remember you. We use this information in order to improve and customize your browsing experience and for analytics and metrics about our visitors both on this website and other media. To find out more about the cookies we use, see our{" "}
-          <a className="underline">
+          <p className="underline">
             Cookie Policy
-          </a>.
+          </p>.
         </p>
       </div>
       <button
