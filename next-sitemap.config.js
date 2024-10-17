28109b266262f805cf4c4f6473eabd5220437247
Erlis Rizqiyani <101242934+erlisrizqiyani@users.noreply.github.com>
2024-10-17 15:20:20 +0700

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://phyanalytica.com',
+    generateRobotsTxt: true, // (optional) Jika Anda ingin juga menghasilkan robots.txt
+    sitemapSize: 7000,
+  }
+  
